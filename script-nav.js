496cd252e15d18faa203b4bc2dc4d8b7059e9824
Matt Miss <matt.w.miss@gmail.com>
2023-07-11 18:00:04 -0700

    Fixed Nav menu
    - Show nav menu in front of everything
    - Navmenu opacity transition added on open/close
    - Changed container img size on small screens

--- script-nav.js
+++ script-nav.js
@@ -1,14 +1,32 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+let navIsOpen = false;
 
 // Activate hamburger menu on hamburger selection
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+    openNavMenu();
 })
 
 // Close navbar on link selection
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    closeNavMenu();
+}))
+
+document.addEventListener("scroll", (e) => {
+    console.log("hello");
+    if (navIsOpen){
+        closeNavMenu();
+    }
+});
+
+function openNavMenu(){
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    navIsOpen = true;
+}
+
+function closeNavMenu(){
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+    navIsOpen = false;
+}
