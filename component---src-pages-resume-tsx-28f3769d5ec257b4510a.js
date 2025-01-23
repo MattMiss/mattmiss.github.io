1e08d31cc00d18538f43db90af1c69fbcb9ea665
Matthew Miss <matt.w.miss@gmail.com>
2025-01-23 14:37:03 -0800

    New Build Deployed

---
+++ component---src-pages-resume-tsx-28f3769d5ec257b4510a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmatt_miss_portfolio=self.webpackChunkmatt_miss_portfolio||[]).push([[194],{3958:function(e,t,i){i.r(t);var n=i(6540);t.default=()=>{const{0:e,1:t}=(0,n.useState)(0);return(0,n.useEffect)((()=>{const e=()=>{t(window.innerHeight)};return e(),window.addEventListener("resize",e),()=>window.removeEventListener("resize",e)}),[]),n.createElement("div",null,n.createElement("embed",{src:"/mattmiss-resume.pdf",type:"application/pdf",width:"100%",height:e}))}}}]);
+//# sourceMappingURL=component---src-pages-resume-tsx-28f3769d5ec257b4510a.js.map
